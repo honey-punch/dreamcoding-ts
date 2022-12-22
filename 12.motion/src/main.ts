82486c6f9eb8ebb7d52d74dda92210d05fc38458
mad <mad@jangjicheol.local>
2022-12-22 14:30:01 +0900

    delete 함수 구현중

--- 12.motion/src/main.ts
+++ 12.motion/src/main.ts
@@ -1,8 +1,5 @@
-const btns = document.querySelectorAll('header .btn');
-const urlBtns = document.querySelectorAll('.url-btn');
-const stringBtns = document.querySelectorAll('.string-btn');
-
-const addBtns = document.querySelectorAll('.add-btn');
+// dialog
+const openBtns = document.querySelectorAll('.open-btn');
 const closeBtns = document.querySelectorAll('.close-btn');
 
 const dialogs = document.querySelectorAll('.dialog');
@@ -20,20 +17,21 @@ function closeDialog(): void {
   dialogs.forEach(e => {e.classList.remove('display')});
 }
 
-btns.forEach(e => e.addEventListener('click', () => {
+openBtns.forEach(e => e.addEventListener('click', () => {
   openDialog(e);
 }));
 closeBtns.forEach(e => {e.addEventListener('click', () => {
   closeDialog()
 })});
 
+// component
+const main = document.querySelector('main');
 
 interface Component {
   generate(): void;
 }
 
 class UrlComponent implements Component {
-  main = document.querySelector('main');
   component = document.createElement('div');
   componentTitle = document.createElement('div');
   iFrame = document.createElement('iframe');
@@ -43,50 +41,19 @@ class UrlComponent implements Component {
   constructor(private title: string, private url: string) {}
 
   generate(): void {
-    this.component.setAttribute('class', 'url-component component');
-    this.componentTitle.setAttribute('class','component-title');
-    this.button.setAttribute('class', 'delete-btn btn');
-    this.icon.setAttribute('class', 'fa-solid fa-xmark');
-    
-    this.main?.appendChild(this.component);
+    main?.appendChild(this.component);
     this.component?.appendChild(this.iFrame)
     this.component?.appendChild(this.componentTitle);
     this.component?.appendChild(this.button);
     this.button?.appendChild(this.icon);
 
-    this.componentTitle.innerText = this.title;
-    this.iFrame.src = this.url;
-  }
-}
-
-class StringComponent implements Component {
-  main = document.querySelector('main');
-  component = document.createElement('div');
-  componentTitle = document.createElement('div');
-  componentDescription = document.createElement('div');
-  button = document.createElement('button');
-  icon = document.createElement('i');
-
-  constructor(private title: string, private description: string) {}
-
-  generate(): void {
-    this.component.setAttribute('class', 'string-component component');
+    this.component.setAttribute('class', 'url-component component');
     this.componentTitle.setAttribute('class','component-title');
-    this.componentDescription.setAttribute('class', 'component-description')
     this.button.setAttribute('class', 'delete-btn btn');
     this.icon.setAttribute('class', 'fa-solid fa-xmark');
     
-    this.main?.appendChild(this.component);
-    this.component?.appendChild(this.componentTitle);
-    this.component?.appendChild(this.componentDescription);
-    this.component?.appendChild(this.button);
-    this.button?.appendChild(this.icon);
-    
     this.componentTitle.innerText = this.title;
-    this.componentDescription.innerText = this.description;
-  }
-  static delete(): void {
-    this.component.remove();
+    this.iFrame.src = this.url;
   }
 }
 
@@ -98,10 +65,38 @@ urlDialog?.addEventListener('submit', (e) => {
   new UrlComponent(inputUrlTitle.value, inputUrl.value).generate();
 });
 
+
+class StringComponent implements Component {
+  component = document.createElement('div');
+  componentTitle = document.createElement('div');
+  componentDescription = document.createElement('div');
+  button = document.createElement('button');
+  icon = document.createElement('i');
+
+  constructor(private title: string, private description: string) {}
+
+  generate(): void {
+    main?.appendChild(this.component);
+    this.component?.appendChild(this.componentTitle);
+    this.component?.appendChild(this.componentDescription);
+    this.component?.appendChild(this.button);
+    this.button?.appendChild(this.icon);
+    
+    this.component.setAttribute('class', 'string-component component');
+    this.componentTitle.setAttribute('class','component-title');
+    this.componentDescription.setAttribute('class', 'component-description')
+    this.button.setAttribute('class', 'delete-btn btn');
+    this.icon.setAttribute('class', 'fa-solid fa-xmark');
+    
+    this.componentTitle.innerText = this.title;
+    this.componentDescription.innerText = this.description;
+  } 
+}
+
 const inputStringTitle = document.getElementById('string-title') as HTMLInputElement;
 const inputString = document.getElementById('string-description') as HTMLInputElement;
 stringDialog?.addEventListener('submit', (e) => {
   e.preventDefault();
   stringDialog.classList.remove('display');
   new StringComponent(inputStringTitle.value, inputString.value).generate();
-});
+}); 
